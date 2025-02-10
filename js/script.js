82ceed53b26bd63f1a5302fb53b5539d51a469f6
Daniel Damasceno <dandamasceno04@gmail.com>
2025-02-10 16:17:36 -0300

    without banner at home, first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Serve para abrir o menu com responsividade.
+$(document).ready(function() {
+    $('#mobile-btn').on('click', function() {
+        $('#mobile-menu').toggleClass('active');
+        $('#mobile-btn').find('i').toggleClass('fa-x');
+    });
+});
